680929d32bf0776cb7a6bb067ce8fbf2921d8ced
sebastiii <sebastiii@team-mediaportal.com>
2013-02-24 13:50:19 +0100

    Fix refresh rate when using LAV Splitter.

--- mediaportal/Core/Player/g_player.cs
+++ mediaportal/Core/Player/g_player.cs
@@ -1321,7 +1321,7 @@ namespace MediaPortal.Player
           if (AskForRefresh)
           {
             // Refreshrate change done here. Blu-ray player will handle the refresh rate changes by itself
-            if (strFile.ToUpper().IndexOf(@"\BDMV\INDEX.BDMV") == -1)
+            if (strFile.IndexOf(@"\BDMV\INDEX.BDMV") == -1)
             {
               RefreshRateChanger.AdaptRefreshRate(strFile, (RefreshRateChanger.MediaType) (int) type);
             }
